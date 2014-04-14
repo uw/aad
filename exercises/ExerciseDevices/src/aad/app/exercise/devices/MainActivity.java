a3c391ea4a24a6ac2ac953b987f4f93a82dafd3b
Mike Maas <mike.maas@gmail.com>
2014-04-14 11:29:01 -0700

    Update to ExerciseDevices
    Correcting the master version of ExerciseDevices

--- exercises/ExerciseDevices/src/aad/app/exercise/devices/MainActivity.java
+++ exercises/ExerciseDevices/src/aad/app/exercise/devices/MainActivity.java
@@ -24,62 +24,23 @@ public class MainActivity extends Activity {
 		
 		StringBuilder sb = new StringBuilder();
 				
-		// Get the Device Information
-		sb.append("Manufacturer: ");
-		sb.append(Build.MANUFACTURER);
-		sb.append(System.getProperty("line.separator"));
-		sb.append("Model: ");
-		sb.append(Build.MODEL);
-		
-		sb.append(System.getProperty("line.separator"));
-		sb.append("SDK: ");
-		sb.append(Build.VERSION.SDK_INT);
+		// TODO 1 Get the Device Information
 		
 		
-		// Check to see if we are running in an emulator
+		// TODO 2 Check to see if we are running in an emulator
 		// Either by looking for google_sdk or vbox86p as the PRODUCT or the FINGERPRINT starting with generic
 		
-		sb.append(System.getProperty("line.separator"));
-		sb.append("Product: ");
-		String product = Build.PRODUCT;
-		sb.append(product);
-				
-		sb.append(System.getProperty("line.separator"));
-		sb.append("Fingerprint: ");
-		String fingerprint = Build.FINGERPRINT;
-		sb.append(Build.FINGERPRINT);
 		
-		sb.append(System.getProperty("line.separator"));
-		if (product.startsWith("vbox") || product.equalsIgnoreCase("google_sdk") || fingerprint.startsWith("generic"))
-			sb.append("This is an Emulator");	
-		else
-			sb.append("This is not an Emulator");
+		// TODO 3 Get the Language
 		
-		// Get the Language
-		sb.append(System.getProperty("line.separator"));
-		sb.append("Default Language: ");
-		sb.append(Locale.getDefault().getDisplayLanguage());
 		
-		// Determine if Facebook is installed
-		sb.append(System.getProperty("line.separator"));		
-		PackageManager pm = getPackageManager();
-		try {
-			pm.getPackageInfo("com.facebook.katana", PackageManager.GET_META_DATA);
-			sb.append("Facebook is installed");
-    	} catch (NameNotFoundException e) {
-    		sb.append("Facebook is not installed");
-    	}
 		
-		// Check to see if we have a camera
-		sb.append(System.getProperty("line.separator"));
-		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    String cameraPackageName = intent.resolveActivity(pm).getPackageName();
-		try {
-			pm.getPackageInfo(cameraPackageName, PackageManager.GET_META_DATA);
-			sb.append("A camera is available");
-    	} catch (NameNotFoundException e) {
-    		sb.append("A camera is not available");
-    	}
+		// TODO 4 Determine if Facebook is installed
+		
+		
+		
+		// TODO 5 Check to see if we have a camera
+		
 		
 		mTextView.setText(sb.toString());
 	}
