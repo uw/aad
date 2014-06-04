5925a4a3b650733a09b730b9f17356bd49ed5d90
Mike Maas <mike.maas@gmail.com>
2014-06-04 14:20:42 -0700

    Updating HelloProGuard
    Including Log stripping example

--- samples/HelloProGuard/src/aad/app/hello/proguard/MainActivity.java
+++ samples/HelloProGuard/src/aad/app/hello/proguard/MainActivity.java
@@ -4,8 +4,11 @@ import aad.app.hello.proguard.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+	private static String TAG = "HelloProGuard";
 
 	/** Called when the activity is first created. */
 	@Override
@@ -16,6 +19,12 @@ public class MainActivity extends Activity {
 		// Create an empty Fragment just to use something from the support library
 		Fragment testFragment = new Fragment();
 		testFragment.setArguments(null);
+		
+		Log.d(TAG, "DEBUG LOGGING");
+		Log.v(TAG, "VERBOSE LOGGING");
+		Log.i(TAG, "INFORMATION LOGGING");
+		Log.w(TAG, "WARN LOGGING");
+		Log.e(TAG, "ERROR LOGGING");
 		
 	}
 }
